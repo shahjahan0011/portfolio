66fa58b98b8f35cad18f8aa5c77537e05bab54dc
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2025-07-19 17:59:51 -0700

    Enhance responsiveness . Adjust layout for smaller screens

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,29 +3,40 @@ import './index.scss';
 import LogoJ from '../../assets/images/logoJ.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faEnvelope, faHome, faSuitcase, faUser, faClose } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
 
 const Sidebar = () => {
+
+    const[showNav, setShowNav] = useState(false);
+
     return (
         <div className="nav-bar">
             <Link to="/" className="logo">
                 <img src={LogoJ} alt="Logo" />
                 <img className="sub-logo" src={LogoSubtitle} alt="Jahan" />
             </Link> 
-            <nav>
-                <NavLink exact="true" activeclassname="active" className="home" to="/">
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="home" to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="about-link" to="/about">
                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink> 
-                <NavLink exact="true" activeclassname="active" className="project-link" to="/project">
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="project-link" to="/project">
                     <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
-                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" className="contact-link" to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
+                <FontAwesomeIcon 
+                onClick={() => setShowNav(false)}
+                icon={faClose}
+                color="#ffd700"
+                size="3x"
+                className='close-icon' />
+            </nav>
                 <ul>
                     <li>
                         <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/jahan-shah-441aa6213/'>
@@ -43,7 +54,9 @@ const Sidebar = () => {
                         </a>
                     </li>
                 </ul>
-            </nav>
+            <FontAwesomeIcon 
+                onClick={() => setShowNav(true)}
+                icon={faBars} color='#ffd700' size='3x' className='burger-icon'/>
 
             </div>
     )
