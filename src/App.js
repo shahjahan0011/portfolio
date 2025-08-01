f5cbf6f07cbe5e165b8616c795ef514251d36987
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2025-08-01 16:20:41 -0700

    add analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Project from './components/Project';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
